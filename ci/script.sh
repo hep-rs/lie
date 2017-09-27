003038b3d0407244e600b1c1997adda959dc1b77
JP-Ellis <josh@jpellis.me>
2017-09-27 13:42:07 +1000

    Export feature flag to CI script
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- ci/script.sh
+++ ci/script.sh
@@ -4,18 +4,14 @@ set -eux
 # Run clippy and see if it has anything to say
 clippy() {
     if [[ "$TRAVIS_RUST_VERSION" == "nightly" && $CLIPPY ]]; then
-        cargo clippy
+        cargo clippy $FEATURES
     fi
 }
 
 # Run the standard build and test suite.
 build_and_test() {
     cargo build
-    if [[ "$TRAVIS_RUST_VERSION" == "nightly" ]]; then
-        cargo test --all-features
-    else
-        cargo test
-    fi
+    cargo test $FEATURES
 }
 
 main() {
--- ci/setup.sh
+++ ci/setup.sh
@@ -3,6 +3,14 @@ set -eux
 
 export PATH=$PATH:~/.cargo/bin
 
+setup_features() {
+    if [[ "$TRAVIS_RUST_VERSION" == "nightly" ]]; then
+        export FEATURES="--all-features"
+    else
+        export FEATURES=""
+    fi
+}
+
 # Check if we are running nightly and install clippy if so.
 clippy() {
     if [[ "$TRAVIS_RUST_VERSION" == "nightly" ]]; then
@@ -12,6 +20,7 @@ clippy() {
 
 main() {
     clippy
+    setup_features
 }
 
 main
